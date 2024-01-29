8499aa2f5ad016a3bc684f75457fbaa5cf5b02f9
smic29 <smic29@DESKTOP-0L09FVV>
2024-01-29 20:12:33 +0800

    Added first database activity

---
+++ activities/database/main.sql
@@ -0,0 +1,24 @@
+CREATE TABLE students (
+	id				INTEGER		PRIMARY KEY,
+	first_name		VARCHAR(50)	NOT NULL,
+	middle_name		VARCHAR(50),
+	last_name		VARCHAR(50)	NOT NULL,
+	age				INTEGER		NOT NULL,
+	location		VARCHAR(50) NOT NULL
+);
+
+INSERT INTO students (id, first_name, last_name, age, location)
+VALUES 
+(1, 'Juan', 'Cruz', 18, 'Manila'),
+(2, 'Anne', 'Wall', 20, 'Manila'),
+(3, 'Theresa', 'Joseph', 21, 'Manila'),
+(4, 'Isaac', 'Gray', 19, 'Laguna'),
+(5, 'Zack', 'Matthews', 22, 'Marikina'),
+(6, 'Finn', 'Lam', 25, 'Manila');
+
+UPDATE students
+SET first_name = 'Ivan', middle_name = 'Ingram', last_name = 'Howard', age = 25, location = 'Bulacan'
+WHERE id = 1;
+
+DELETE FROM students
+WHERE id = (SELECT MAX(id) FROM students);
