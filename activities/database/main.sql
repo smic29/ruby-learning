4030643a092bad99e3c21fa026a72be4e0fd9457
smic29 <smic29@DESKTOP-0L09FVV>
2024-01-31 19:48:48 +0800

    Added activity 3 code in main.sql

--- activities/database/main.sql
+++ activities/database/main.sql
@@ -32,3 +32,48 @@ SELECT AVG(age) AS average_student_age from students;
 
 SELECT * FROM students
 ORDER BY age DESC;
+
+-- Start of Activity 3
+
+SELECT * FROM research_papers; --TABLE CHECK LINE
+
+CREATE TYPE valid_grades AS ENUM ('A','B','C','D','E','F');
+
+CREATE TABLE research_papers (
+	id int NOT NULL PRIMARY KEY,
+	student_id int,
+	grade valid_grades,
+	FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+INSERT INTO research_papers (id, student_id, grade)
+VALUES 
+(6, 1, 'A'),
+(7, 2, 'A'),
+(8, 3, NULL),
+(9, 4, 'E'),
+(10, 5, 'F'),
+(11, 1, 'D'),
+(12, 3, 'A'),
+(13, 2, 'C'),
+(14, 1, 'B'),
+(15, 2, NULL)
+;
+
+SELECT 
+	CONCAT(UPPER(s.last_name),' ',s.first_name) AS name,
+	COUNT(r.grade) AS number_of_research_papers
+FROM students AS s 
+JOIN research_papers AS r
+ON r.student_id = s.id
+GROUP BY name 
+ORDER BY number_of_research_papers DESC;
+
+SELECT
+	CONCAT(UPPER(s.last_name),' ',s.first_name) AS name,
+	r.id,
+	r.grade
+FROM students AS s 
+JOIN research_papers AS r 
+ON r.student_id = s.id
+WHERE r.grade IS NULL;
