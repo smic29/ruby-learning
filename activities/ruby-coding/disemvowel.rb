52d26ccee4da7344502574ddcf8b21747ec53d44
smic29 <smic29@DESKTOP-0L09FVV>
2024-02-03 09:34:28 +0800

    Included a non .gsub method answer for disemvowel.rb

--- activities/ruby-coding/disemvowel.rb
+++ activities/ruby-coding/disemvowel.rb
@@ -1,7 +1,15 @@
 def disemvowel(text)
-  text.gsub(/[aeiou]/i,'')
+  # text.gsub(/[aeiou]/i,'')
 
+  # without using .gsub method
 
+  vowels = /[aeiou]/i
+  modified_text = [];
+  text.each_char do |char|
+    modified_text << char unless char =~ vowels
+  end
+
+  modified_text.join()
 end
 
 puts disemvowel('This website is for losers LOL!')
